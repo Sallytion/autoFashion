42a18a5971d7ebf4903f166998a27e7992a43e3e
Sallytion <Yash.tekwani2022@vitstudent.ac.in>
2024-03-19 18:21:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,142 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import ThreeScene from './ThreeScene';
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
+
+function App() {
+  const [color, setColor] = useState(localStorage.getItem('color') || '#ffffff');
+  const [image, setImage] = useState(null);
+  const [imgPos, setImgPos] = useState({x: 0, y: 0});
+  const [imgScale, setImgScale] = useState(1);
+  const [rotateImg, setImgRotation] =  useState(1);
+  const [crop, setCrop] = useState({ aspect: 1 });
+  const [croppedImage, setCroppedImageUrl] = useState(null);
+
+  let imgRot = 0;
+
+  const onImageLoaded = (image) => {
+    this.imageRef = image;
+  };
+
+  const onCropComplete = (crop) => {
+    makeClientCrop(crop);
+  }
+
+  const onCropChange = (crop,percentCrop) => {
+    setCrop(crop);
+  }
+
+  const makeClientCrop = async (crop) => {
+    if (this.imageRef && crop.width && crop.height) {
+      const croppedImageUrl = await getCroppedImg(
+        this.imageRef,
+        crop,
+        'newFile.jpeg'
+      );
+      setCroppedImageUrl(croppedImageUrl);
+    }
+  };
+
+  const getCroppedImg = (image, crop, fileName) => {
+    const canvas = document.createElement('canvas');
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(
+      image,
+      crop.x * scaleX,
+      crop.y * scaleY,
+      crop.width * scaleX,
+      crop.height * scaleY,
+      0,
+      0,
+      crop.width,
+      crop.height
+    );
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(blob => {
+        if (!blob) {
+          //reject(new Error('Canvas is empty'));
+          console.error('Canvas is empty');
+          return;
+        }
+        blob.name = fileName;
+        resolve(window.URL.createObjectURL(blob));
+      }, 'image/jpeg');
+    });
+  };
+
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  };
+
+  const handleImageUpload = (event) => {
+    setImage(URL.createObjectURL(event.target.files[0]));
+  }
+
+  const handleRotation = (degree) => {
+    const radian = degree * (Math.PI / 180); // Convert degree to radian
+    setImgRotation(prevRotation => prevRotation + radian);
+  }
+  const handlePosition = (incrementX, incrementY) => {
+    setImgPos(prevPos => ({x: prevPos.x + incrementX, y: prevPos.y + incrementY}));
+  }
+  const handleScale = (scaleChange) => {
+    setImgScale(prevScale => {
+      const newScale = prevScale + scaleChange;
+      console.log(newScale);
+      return newScale;
+    });
+  }
+
+  const handleCrop = () => {
+    setImage(croppedImageUrl);
+  };
+
+  // useEffect(() => {
+  //   localStorage.setItem('color', color);
+  // }, [color]);
+
+  return (
+    <div className="container">
+      <ThreeScene color={color} textureUrl={image} textureOffset={imgPos} textureScale={imgScale} textureRotation={rotateImg} />
+      <div className="other-side element-with-border">
+        <input type="color" value={color} onChange={handleColorChange} />
+        <br />
+        <input type="file" onChange={handleImageUpload}/>
+        <br />
+        {image && (
+          <>
+            <ReactCrop
+            src={image}
+            crop={crop}
+            onImageLoaded={onImageLoaded}
+            onComplete={onCropComplete}
+            onChange={onCropChange}
+            />
+            <br />
+            <img src={image} alt="Uploaded content" width="50%"/>
+            <br />
+            <div style={{ width: '50%', margin: 'auto' }}>
+              <button style={{ padding: '10px', margin: '5px' }} onClick={() => handleRotation(5)}>Rotate Texture</button>
+              <button style={{ padding: '10px', margin: '5px' }} onClick={() => handlePosition(0.1, 0)}>Position X++</button>
+              <button style={{ padding: '10px', margin: '5px' }} onClick={() => handlePosition(0, 0.1)}>Position Y++</button>
+              <button style={{ padding: '10px', margin: '5px' }} onClick={() => handlePosition(-0.1, 0)}>Position X--</button>
+              <button style={{ padding: '10px', margin: '5px' }} onClick={() => handlePosition(0, -0.1)}>Position Y--</button>
+              <button style={{ padding: '10px', margin: '5px' }} onClick={() => handleScale(0.01)}>Scale++</button>
+              <button style={{ padding: '10px', margin: '5px' }} onClick={() => handleScale(-0.01)}>Scale--</button>
+              <button stype={{ padding: '10px', margin: '5px' }} onClick={() => handleCrop()}>Crop Image</button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ThreeScene.jsx
@@ -0,0 +1,81 @@
+import { Canvas, useThree, extend, useFrame } from '@react-three/fiber';
+import { useGLTF } from '@react-three/drei';
+import tshirtModel from './assets/only-tshirt.glb';
+import { DoubleSide } from 'three';
+import { MeshPhysicalMaterial, MeshStandardMaterial } from 'three';
+import { OrbitControls as ThreeOrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { useRef } from 'react';
+import { TextureLoader } from 'three';
+import { useEffect } from 'react';
+
+extend({ OrbitControls: ThreeOrbitControls });
+
+const OrbitControls = (props) => {
+    const {
+        camera,
+        gl: { domElement },
+    } = useThree();
+
+    const controls = useRef();
+    useFrame((state) => controls.current.update());
+
+    return (
+        <orbitControls
+            ref={controls}
+            args={[camera, domElement]}
+            minDistance={0.8}
+            maxDistance={2}
+            {...props}
+        />
+    );
+};
+
+function ThreeScene({ color = 'white', textureUrl, textureOffset, textureRotation, imgScale }) {
+    return (
+        <Canvas camera={{ position: [0,0.5,1]}}>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Model url={tshirtModel} position={[0,-1.3,0]} color={color} textureUrl={textureUrl} textureOffset={textureOffset} textureRotation={textureRotation} imgScale={imgScale}/>
+            <OrbitControls />
+        </Canvas>
+    );
+}
+
+function Model({url, position, color = 'white', textureUrl, textureOffset = {x: 0, y: 0}, textureRotation = 1, imgScale = 1}){
+    const gltf = useGLTF(url,true);
+    const { scene } = gltf;
+    let texture = null;
+
+    if (textureUrl) {
+        texture = new TextureLoader().load(textureUrl);
+
+        // Set the texture offset
+        texture.offset.set(textureOffset.x, textureOffset.y);
+
+        // Set the texture rotation
+        texture.rotation = textureRotation;
+
+        // Set the texture scale
+        texture.repeat.set(imgScale, imgScale);
+    }
+
+    scene.traverse((child) => {
+        if (child.isMesh) {
+            child.material = new MeshPhysicalMaterial({
+                map: texture,
+                color: color,
+                reflectivity: 1,
+                clearcoat: 1.0,
+                side: DoubleSide
+            });
+        }
+    });
+    useEffect(()=>{
+        if(texture){
+            texture.repeat.set(imgScale, imgScale);
+        }
+    })
+    return <primitive object={scene} position={position} dispose={null}/>;
+}
+
+export default ThreeScene;
